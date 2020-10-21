fb083e048c76a8f32f2b5e129fe0d47aaf768f97
somendrayuva293 <somendrayuva293@gmail.com>
2020-10-21 17:47:13 +0530

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_075806) do
+ActiveRecord::Schema.define(version: 2020_10_21_072910) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "assemblies", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
